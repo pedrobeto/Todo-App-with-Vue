54b786f1cf5ab6cacff6a73fc5a31a4ef5277418
Pedro Beda <pbeda@soujmv.com>
2020-12-28 16:20:04 -0300

    stucked on props to parent

---
+++ todo/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+            id: 1,
+            title: "Todo one",
+            completed: false,
+        },
+        {
+            id: 2,
+            title: "Todo two",
+            completed: true,
+        },
+        {
+            id: 3,
+            title: "Todo three",
+            completed: false,
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: Arial, Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ todo/src/components/Todos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" @isCompletedChanged="todo.completed = $isCompletedChanged" />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"],
+    methods: {
+        isCompletedChanged() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
